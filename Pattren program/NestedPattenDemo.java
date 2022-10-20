8dcf3d3bc25436a8bd7c0922d40364b524f8d8b1
Riteshrp1 <pradhanritesh199@gmail.com>
2022-10-20 23:25:26 +0530

    Pattern program

---
+++ Pattren program/NestedPattenDemo.java
@@ -0,0 +1,21 @@
+class NestedPattenDemo{
+
+public static void main(String[] args){
+
+int n = 6;
+for(int i=1; i<n; i++){
+
+for(int j = i; j<n; j++){
+if(i==1 || j==i || j==n-1){
+System.out.print("* ");
+}
+  else{
+   System.out.print(" "+" ");
+  } 
+
+}  
+  System.out.println( );
+}
+}
+
+}
