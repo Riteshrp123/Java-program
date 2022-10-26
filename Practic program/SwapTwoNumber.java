660040e38093d193a5ba8dff24f914912adb0ec2
Riteshrp1 <pradhanritesh199@gmail.com>
2022-10-26 21:13:55 +0530

    Practic program

---
+++ Practic program/SwapTwoNumber.java
@@ -0,0 +1,19 @@
+public class SwapTwoNumber{
+
+public static void main(String[] args){
+
+int x = 10,y = 20;
+System.out.println("Values before Swapping:");
+
+System.out.println("x = " +x+" y = "+y);
+
+x = x*y; 
+y = x/y; 
+x = x/y; 
+
+System.out.println("Values after Swapping:");
+
+System.out.println("x = " +x+" y = "+y);
+
+}
+}
