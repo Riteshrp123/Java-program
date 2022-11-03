6b62d5e2707fccc81a47cca788a04e08b3823ad4
Riteshrp1 <pradhanritesh199@gmail.com>
2022-11-03 14:17:07 +0530

    Oops concept

---
+++ Oops concept/polymorphism1/methode_overLoding/multiply/MethodOverLoding.java
@@ -0,0 +1,12 @@
+package com.xworkz.corejava.polymorphism1.methode_overLoding.multiply;
+
+public class MethodOverLoding {
+
+	public static void main(String[] args) {
+		
+		Multiply m = new Multiply();
+		
+		m.mul(4, 6);
+		m.mul(6, 8, 10);
+	}
+}
---
+++ Oops concept/polymorphism1/methode_overLoding/multiply/Multiply.java
@@ -0,0 +1,18 @@
+package com.xworkz.corejava.polymorphism1.methode_overLoding.multiply;
+
+public class Multiply {
+	
+	void mul(int a,int b) {
+		
+		System.out.println(a*b);
+		
+	}
+	
+	void mul(int a,int b,int c) {
+		
+		System.out.println(a*b*c);
+		
+		
+	}
+
+}
