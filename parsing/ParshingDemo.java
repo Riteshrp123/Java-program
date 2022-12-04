c4a758a981ca70625a1bcf9e7c1f0b3f6390948e
Riteshrp1 <pradhanritesh199@gmail.com>
2022-12-04 11:53:31 +0530

    parsing

---
+++ parsing/ParshingDemo.java
@@ -0,0 +1,16 @@
+package com.xworkz.corejava.parsing;
+
+public class ParshingDemo {
+	public static void main(String[] args) {
+		
+		String s1 = "10";
+		String s2 = "20";
+		
+		System.out.println(s1+s2);
+		 
+		Integer s3 = Integer.parseInt(s1);
+		Integer s4 = Integer.parseInt(s2);
+		System.out.println(s3+s4);
+	}
+
+}
